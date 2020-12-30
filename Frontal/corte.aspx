18e253fd773ca852205273d20c98cabea14df3d9
Your Name <crizalit21@outlook.com>
2020-12-30 09:53:25 -0600

    prueba

---
+++ Frontal/corte.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Frontal/Site1.Master" AutoEventWireup="true" CodeBehind="corte.aspx.cs" Inherits="NOMINA23.WebForm3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="~/Frontal/estilos/estilos_corte.css" media="screen" />
+
+     <style type="text/css">
+         /*#ContentPlaceHolder1_calFcorte {
+             display:flex;
+             justify-content:center;
+             ju
+         }*/
+         .fecha_t {
+             display: flex;
+             align-items:center;
+             
+
+         }
+         .calendario {
+             display: flex;
+             align-items:center;
+         }
+         .principal {
+             
+             padding-top:4%;
+         }
+         .tabla {
+           
+             background-color:whitesmoke;
+             text-align:left;
+             /*width:100%;*/
+             border-collapse:collapse;
+         }
+         .contine_table {
+             border-radius: 20px;
+             display:flex;
+             justify-content:center;
+         }
+         .th_t, .td_t {
+             padding: 20px;
+         }
+         .encabezado_tabla {
+             border-radius: 20px;
+             background-color: #800040;             
+             border-bottom: solid 5px #800060;
+             color: white;
+         }
+             
+         }
+         .total {
+             background-color: seagreen;
+             border-top:solid 5px blue;
+         }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="principal">
+
+   
+          <div>
+       <p class="fecha_t">
+           Fecha de corte
+       </p> 
+           
+                <asp:Calendar runat="server" ID="calFcorte"></asp:Calendar>
+
+
+        <div class="calendario">
+            <asp:Button ID="genera_corte" runat="server" Text="Generar" OnClick="genera_corte_Click" />
+        </div>
+    </div>
+    <div class="contine_table">
+
+        <table class="tabla">
+            <tr class="encabezado_tabla">
+                <th class="th_t">Empleado</th>
+                <th class="th_t">Monto</th>
+                <th class="th_t">Articulos</th>
+            </tr>
+            <% Response.Write(tablaPres); %>            
+            <% Response.Write(total); %>
+            
+             
+        </table>
+
+
+    </div>
+         </div>
+    
+  
+</asp:Content>
---
+++ Frontal/corte.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using System.Drawing;
+
+
+namespace NOMINA23
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        private conectarid cbd = new conectarid();
+        private string fecha_corte;
+        public string tablaPres = "";
+        public string total = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void genera_corte_Click(object sender, EventArgs e)
+        {
+            fecha_corte = calFcorte.SelectedDate.ToString();
+            
+            string datReci = null;/*aqui recibiremos los datos*/
+            string secReg = "";/*secmento por registro aqui*/
+            string secCad = "";/*cada uno tiene un atributo*/
+            /*controlaremos el siclo or la longitud de cadena y registro*/
+            int lonReg = 0, lonCad = 0, caso = 0;
+            double totalar=0,totalmon =0;
+            // System.Windows.Forms.ListViewItem lista = new System.Windows.Forms.ListViewItem();
+             
+
+
+
+
+            datReci =cbd.genera_corte(fecha_corte);
+            lonCad = datReci.Length;/*cuarda la longitud de la cadena que reciobio de la DB*/
+            
+            while (lonCad > 1)
+            {
+                tablaPres += "<tr>";/*abre renglon*/
+                secReg = datReci.Substring(0, datReci.IndexOf("$"));/*guarda el primer registro  en secreg*/
+                /*cortamos la cadena que ya tomamos*/
+                datReci = datReci.Substring(datReci.IndexOf("$") + 1);
+                lonReg = secReg.Length;/*longitud del registro*/
+                while (lonReg > 1)/*para partir el registro*/
+                {
+                    secCad = secReg.Substring(0, secReg.IndexOf("|"));
+                    secReg = secReg.Substring(secReg.IndexOf("|") + 1);
+                    
+                    switch (caso) {
+                        case 0:
+                            tablaPres += "<td class=\"td_t\">" + secCad + "</td>";
+                            
+                            break;
+                        case 1:
+                            tablaPres += "<td class=\"td_t\">" + secCad + "</td>";
+                            totalmon=totalmon+Double.Parse(secCad);
+                            break;
+                        case 2:
+                            tablaPres += "<td class=\"td_t\">" + secCad + "</td>";
+                            totalar = totalar + Double.Parse(secCad);
+                            break;
+                    }caso++;
+                                       
+                    lonReg = secReg.Length;/*longitud de la cadena despues del corte*/
+                }
+                tablaPres += "</tr>";
+                caso = 0;
+                lonCad = datReci.Length;
+            }
+
+            total = "<tr  class=\"total\">" + "<th class=\"th_t\">" + "Total"+ "</th>"+ "<th class=\"th_t\">" + Convert.ToString(totalmon) + "</th>"+ "<th class=\"th_t\">" + Convert.ToString(totalar) + "</th>" + "</tr>";
+        }
+
+        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
